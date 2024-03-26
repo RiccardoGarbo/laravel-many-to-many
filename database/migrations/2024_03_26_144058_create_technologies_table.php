dc270f40c26a3ddf7bad4bd297f922a573b5917c
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-26 16:11:02 +0100

    create table technologies with migrate

--- database/migrations/2024_03_26_144058_create_technologies_table.php
+++ database/migrations/2024_03_26_144058_create_technologies_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('label')->unique();
+            $table->string('color')->nullable();
             $table->timestamps();
         });
     }
